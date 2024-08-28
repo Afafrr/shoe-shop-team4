c89eeea21aec2a9757340186a676c43d2b6ed0cf
beckman924 <braiancarabajal@gmail.com>
2024-08-28 20:34:45 -0300

    feat: Added Input and Form components

---
+++ app/sign-up/actions.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { signUpSchema } from "@/schemas/signUpSchema";
+
+export type FormState = {
+  data: Object;
+  error?: {
+    message: string;
+  };
+};
+
+export async function signUp(data: FormData): Promise<FormState> {
+  try {
+    const formData = Object.fromEntries(data);
+    const parsed = signUpSchema.safeParse(formData);
+
+    if (!parsed.success) {
+      return {
+        data: {},
+        error: {
+          message: "Invalid form data",
+        },
+      };
+    }
+
+    const { name: username, email, password } = parsed.data;
+    const response = await fetch(
+      `${process.env.BASE_URL}/auth/local/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      }
+    );
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    return {
+      data: {},
+      error: {
+        message: "Something went wrong",
+      },
+    };
+  }
+}
--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,4 +1,6 @@
-import { Button, Stack } from "@mui/material";
+"use client";
+
+import { Button, Stack, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
 import {
   FormContainer,
@@ -7,8 +9,11 @@ import {
 } from "react-hook-form-mui";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 
 import Input from "@components/Input/Input";
+import { FormState } from "@/app/sign-up/actions";
+import WarningIcon from "./WarningIcon";
 
 export interface FormInput {
   label: string;
@@ -21,7 +26,7 @@ export default function Form({
   schema,
 }: {
   inputs: FormInput[];
-  submitFn: (data: FieldValues) => void;
+  submitFn: (data: FormData) => Promise<FormState>;
   schema: z.ZodSchema<FieldValues>;
 }) {
   const defaultValues = inputs.reduce(
@@ -36,24 +41,66 @@ export default function Form({
     defaultValues,
     resolver: zodResolver(schema),
   });
-  const {
-    handleSubmit,
-    formState: { isSubmitting },
-  } = formContext;
+  const { handleSubmit, reset } = formContext;
+
+  const { mutate, data, error, isPending } = useMutation<
+    FormState,
+    Error,
+    FieldValues
+  >({
+    mutationKey: ["signUp"],
+    mutationFn: async (data: FieldValues) => {
+      const formData = new FormData();
+
+      Object.entries(data).forEach(([key, value]) => {
+        formData.append(key, value as string);
+      });
+
+      const result = await submitFn(formData);
+      return result;
+    },
+    onSuccess: ({ data }) => {
+      console.log(data);
+      // reset();
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
   return (
     <FormContainer
       formContext={formContext}
-      handleSubmit={handleSubmit(submitFn)}
+      handleSubmit={handleSubmit((data) => mutate(data))}
     >
       <Stack spacing={"24px"}>
+        {error || data ? (
+          <Typography
+            color={"red"}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
+              fontSize: "16px",
+              fontWeight: 400,
+            }}
+          >
+            <WarningIcon /> {error?.message || data?.error?.message}
+          </Typography>
+        ) : null}
+
         {inputs.map((input) => (
-          <Input key={input.label} label={input.label} props={input.props} />
+          <Input
+            key={input.label}
+            label={input.label}
+            props={input.props}
+            disabled={isPending}
+          />
         ))}
 
         <Button
           type="submit"
-          disabled={isSubmitting}
+          disabled={isPending}
           sx={{
             py: "25px",
             borderRadius: "5.58px",
