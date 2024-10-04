2587f9fa5f5d83b2b3149f3f595602a9dd9038ef
Afafrr <mateusz.aftanski@gmail.com>
2024-10-04 10:00:31 +0200

    Whole app test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
     },
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverageFrom: ["<rootDir>/app/**/*.{js,ts,tsx}"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
