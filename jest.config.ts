7339dd74def02214d7f224d5c5efaa64f963e515
Afafrr <mateusz.aftanski@gmail.com>
2024-10-17 08:22:00 +0200

    tests: CheckoutStripeForm successes and errors

--- jest.config.ts
+++ jest.config.ts
@@ -22,6 +22,9 @@ const config: Config = {
     },
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
