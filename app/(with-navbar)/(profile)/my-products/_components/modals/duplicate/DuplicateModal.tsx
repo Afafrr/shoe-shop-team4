3828295e91e522fa448721ce2174497c270df068
Marino3005 <marino.ramon@usal.edu.ar>
2024-10-17 19:49:43 -0300

    feat - add duplicate functionality

--- app/(with-navbar)/(profile)/my-products/_components/modals/duplicate/DuplicateModal.tsx
+++ app/(with-navbar)/(profile)/my-products/_components/modals/duplicate/DuplicateModal.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { productSchema } from "@/schemas/productSchema";
-import { editProductAction } from "./action";
 import { EditProduct } from "@/types/Product";
 import { ContextType } from "@/types/types";
 import { emptyFormValues } from "../../ProductManager/ProductForm/FormPage";
 import { useQueryClient } from "@tanstack/react-query";
 import successToast from "@/components/Alerts/successToast";
+import { addProductAction } from "../../../new/action";
 import ProductModal from "../ProductModal";
 
-// This is a modal that pops when the user clicks on edit product. Uses same template as AddProduct page: 'ProductManager.tsx'
+// This is a modal that pops when the user clicks on Duplicate product. Uses same template as AddProduct page: 'ProductManager.tsx'
 // This component takes:
 //    `open` and `handleClose` props to manage the modal's state
-//    `Product`, a product of type 'EditProduct' with all properties to fill default form, and an additional `Id` for backend request.
+//    `Product`, a product of type 'EditProduct' with all properties to fill default form,
+//    `id` property present in type `EditProduct` is unnecessary here.
 
 type ModalType = {
   open: boolean;
@@ -19,13 +20,17 @@ type ModalType = {
   product: EditProduct | undefined;
 };
 
-export default function EditModal({ open, handleClose, product }: ModalType) {
+export default function DuplicateModal({
+  open,
+  handleClose,
+  product,
+}: ModalType) {
   const { id, ...formProduct } = product ?? { id: "", ...emptyFormValues };
   const defaultValues = formProduct;
   const queryClient = useQueryClient();
 
   const title = {
-    header: "Edit Product",
+    header: "Duplicate Product",
     subheader:
       "Update product names, adjust pricing, and assign the correct brand and categories. Easily manage product details like colors, sizes, and gender to ensure accurate listings. Upload images and fine-tune descriptions to present your products in the best light.",
   };
@@ -33,12 +38,12 @@ export default function EditModal({ open, handleClose, product }: ModalType) {
   const formActions = {
     schema: productSchema,
     submitAction: (formData: FormData, context: ContextType) => {
-      return editProductAction(formData, context, id);
+      return addProductAction(formData, context);
     },
     successFn: async () => {
       queryClient.invalidateQueries({ queryKey: ["my-products"] });
       queryClient.invalidateQueries({ queryKey: ["productDetails"] });
-      successToast("Product edited successfully!");
+      successToast("Product added successfully!");
       return handleClose();
     },
     defaultForm: defaultValues,
