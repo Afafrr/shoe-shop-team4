055b7a0d2db3fca65342b1832af6800ca833dc2b
Afafrr <mateusz.aftanski@gmail.com>
2024-10-23 12:06:20 +0200

    removed unused imports

--- __tests__/app/(with-navbar)/checkout/page.test.tsx
+++ __tests__/app/(with-navbar)/checkout/page.test.tsx
@@ -1,8 +1,7 @@
 import Page from "@/app/(with-navbar)/checkout/page";
-import { getCustomerData } from "@/app/(with-navbar)/checkout/serverActions";
 import { QueryClient } from "@tanstack/react-query";
 import { render, screen, waitFor } from "@testing-library/react";
-import { HydrationBoundary } from "@tanstack/react-query";
+
 jest.mock("@/app/(with-navbar)/checkout/_components/ClientPage", () =>
   jest.fn(() => <div>ClientPage</div>)
 );
